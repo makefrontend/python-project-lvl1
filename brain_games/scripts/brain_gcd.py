e44a290b52c97d02db8234171c182139b87a9b10
Anton Rojdestvensky <makefrontend@ya.ru>
2021-08-03 00:42:09 +0300

    hotfix

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -5,9 +5,10 @@ import prompt
 
 NUMBERS_ROUND = 3
 
+
 def get_gcd(num1, num2):
-    while num2 != 0: 
-        num1, num2 = num2, num1 % num2 
+    while num2 != 0:
+        num1, num2 = num2, num1 % num2
     return num1
 
 
